004ff0de00af58d310a48064395c6689e88b7347
malcolmcusack <cusackmalk@gmail.com>
2020-09-15 21:14:43 -0600

    created get and post api endpoints and was as the schema for tinder cards. Also configured mongoose database with express and Cors middlewares.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from 'mongoose';
+import Cards from './dbCards.js';
+import Cors from 'cors';
 
 // app config
 const app = express();
@@ -7,6 +9,8 @@ const port = process.env.PORT || 8001
 const connection_url = "mongodb+srv://admin:ekLGQneW2vCdtXD@cluster0.otevd.mongodb.net/tinder-cloneDB?retryWrites=true&w=majority"
 
 // middlewares
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -17,6 +21,28 @@ mongoose.connect(connection_url, {
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello World'));
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    });
+});
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
 
 
 // Listener
